ba23689622553da7a97b08b819995a6f6412c435
unknown <sunpeng@Unicloud.com>
2019-07-31 14:27:31 +0800

    implement export tree information to file system

--- src/Main.java
+++ src/Main.java
@@ -19,33 +19,28 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.List;
 
 public class Main extends JPanel implements ActionListener, ItemListener {
     JFileChooser chooser;
     JScrollPane scroll;
     JTree tree;
-    JTree treeBackUp;
     JTextField text;
     JButton button;
     FileReader reader;
     DefaultTreeModel treeModel;
     DefaultMutableTreeNode rootNode;
-    JComboBox choice;
-    String keyWord;
+    String[] keyWord ;
     String action;
     String logPath = null;
     Boolean changed =false ;
 
     public static String CMD_FILTER = "filter";
     public static String CMD_SHOW = "show";
-    public static String CMD_ALL = "all";
+    public static String CMD_EXPORT = "export";
     public static String CMD_OPEN = "open";
 
 
@@ -75,23 +70,17 @@ public class Main extends JPanel implements ActionListener, ItemListener {
         button.addActionListener(this);
         pTop.add(button,BorderLayout.NORTH);
 
-        text = new JTextField(10);
+        text = new JTextField(30);
 
         JButton b1 = new JButton("过滤");
         JButton b2 = new JButton("显示相关");
-        JButton b3 = new JButton("显示全部内容");
+        JButton b3 = new JButton("导出");
         b1.setActionCommand(CMD_FILTER);
         b2.setActionCommand(CMD_SHOW);
-        b3.setActionCommand(CMD_ALL);
+        b3.setActionCommand(CMD_EXPORT);
         b1.addActionListener(this);
         b2.addActionListener(this);
         b3.addActionListener(this);
-
-
-        choice = new JComboBox<>();
-        choice.addItem("只显示与其相关");
-        choice.addItem("过滤掉与其相关");
-        choice.addItemListener(this);
 
         pTop.add(text,BorderLayout.SOUTH);
         pTop.add(b1,BorderLayout.WEST);
@@ -109,7 +98,7 @@ public class Main extends JPanel implements ActionListener, ItemListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String action = e.getActionCommand();
+        action = e.getActionCommand();
         switch (action){
             case "filter":
                 treeFilter(CMD_FILTER);
@@ -117,8 +106,8 @@ public class Main extends JPanel implements ActionListener, ItemListener {
             case "show":
                 treeFilter(CMD_SHOW);
                 break;
-            case "all":
-                treeFilter(CMD_ALL);
+            case "export":
+                checkNode(rootNode);
                 break;
             case "open":
                 readLog();
@@ -130,25 +119,48 @@ public class Main extends JPanel implements ActionListener, ItemListener {
     }
 
     public void treeFilter(String type){
-        keyWord = text.getText().trim();
-        if(logPath ==null || keyWord.length()==0){
+        if(text.getText().trim().indexOf("|")>0){
+            keyWord = text.getText().trim().split("\\|");
+        }
+        else {
+            keyWord = new String[1];
+            keyWord[0] = text.getText().trim();
+ //           keyWord = new String[]{text.getText().trim()};
+        }
+        if(logPath ==null || keyWord[0].equals("")){
             return;
         }
-        String reg = ".*"+"(?i)"+keyWord+".*";
+        String reg="";
         if(changed == false){
+            Boolean hasKeyword=false,tmp=false;
             Enumeration ee=rootNode.children();
             List<Object> list= Collections.list(ee);
-
             if(type =="filter"){
+                setBackground(Color.cyan);
                 for(Object o :list){
-                    if(       o.toString().matches(reg)      ){
+                    for(String str: keyWord){
+                        if(str!=null){
+                            reg = ".*"+"(?i)"+str+".*";
+                            tmp = o.toString().matches(reg);
+                            hasKeyword = tmp ||hasKeyword;
+                            reg = "";
+                        }
+                    }
+                    if(hasKeyword){
                         rootNode.remove((MutableTreeNode) o);
+                        //重新赋值为false.避免为true时的错误
+                        hasKeyword=false;
                     }
                 }
             }else if(type == "show"){
-                for(Object o :list){
-                    if(        !(o.toString().matches(reg))  ){
-                        rootNode.remove((MutableTreeNode) o);
+                if(text.getText().trim().indexOf("|")>0){
+                    JOptionPane.showMessageDialog(this,"暂不支持","提示",JOptionPane.INFORMATION_MESSAGE);
+                }else {
+                    reg = ".*"+"(?i)"+keyWord[0]+".*";
+                    for(Object o :list){
+                        if(        !(o.toString().matches(reg))  ){
+                            rootNode.remove((MutableTreeNode) o);
+                        }
                     }
                 }
             }
@@ -158,6 +170,10 @@ public class Main extends JPanel implements ActionListener, ItemListener {
             //重新读取文件.并且逐行过滤
             System.out.println("此时需要重新读取文件");
             System.out.println(logPath);
+            if(   type == "show" && keyWord.length > 1  ) {
+                JOptionPane.showMessageDialog(this, "暂不支持", "提示", JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
             clear();
             File file = new File(logPath);
             try{
@@ -174,17 +190,24 @@ public class Main extends JPanel implements ActionListener, ItemListener {
                         node = addObject(rootNode,s,false);
                         parent = (DefaultMutableTreeNode) node.getParent();
                         if(parent.getIndex(node)>0){
-
+                            //tmp节点就是当前节点的上一个节点
                             tmp = (DefaultMutableTreeNode) parent.getChildAt(   parent.getIndex(node)-1   );
                             if(    type =="filter" ){
-                                if(  tmp.getUserObject().toString().matches(reg) ){
-                                    System.out.println("00");
+                                Boolean hasKeyword=false,tmpHasKey=false;
+                                for(String str: keyWord){
+                                    if(str!=null){
+                                        reg = ".*"+"(?i)"+str+".*";
+                                        tmpHasKey = tmp.getUserObject().toString().matches(reg);
+                                        hasKeyword = tmpHasKey ||hasKeyword;
+                                    }
+                                }
+                                if(hasKeyword){
                                     rootNode.remove(tmp);
                                 }
                             }else if(   type == "show"  ){
-                                System.out.println(keyWord);
-                                if(  !tmp.getUserObject().toString().matches(reg) ){
-                                    rootNode.remove(tmp);
+                                    reg = ".*"+"(?i)"+keyWord[0]+".*";
+                                    if(  !tmp.getUserObject().toString().matches(reg) ){
+                                        rootNode.remove(tmp);
                                 }
                             }
                         }
@@ -192,6 +215,7 @@ public class Main extends JPanel implements ActionListener, ItemListener {
                         addObject(node,s,false);
                     }
                 }
+                tree.expandPath(new TreePath(rootNode.getPath()) );
             }catch (Exception fileNotFountException){
                 System.out.println(fileNotFountException.toString());
             }
@@ -207,6 +231,7 @@ public class Main extends JPanel implements ActionListener, ItemListener {
             String fileName = chooser.getSelectedFile().getName();
             logPath = chooser.getSelectedFile().getAbsolutePath();
             rootNode.setUserObject(fileName);
+ //           tree.scrollPathToVisible(new TreePath(rootNode));
             File file = new File(dir,fileName);
             try{
                 reader = new FileReader(file);
@@ -225,6 +250,8 @@ public class Main extends JPanel implements ActionListener, ItemListener {
                         addObject(node,s,false);
                     }
                 }
+                //tree.scrollPathToVisible(new TreePath(rootNode.getPath()));
+                tree.expandPath(new TreePath(rootNode.getPath()) );
 
             }catch (Exception e2){
                 System.out.println(e2.toString());
@@ -232,10 +259,62 @@ public class Main extends JPanel implements ActionListener, ItemListener {
         }
     }
 
+    public void checkNode(DefaultMutableTreeNode node){
+        String writeFilePath = "C:\\Users\\sunpeng\\Documents\\";
+        Date date = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("yyy-MM-dd-hh-mm-ss");
+
+        String writeFileName = ft.format(date)+".log";
+
+        String writeFileFullPath = writeFilePath + writeFileName;
+        File writeFile = new File(writeFileFullPath);
+        try{
+            if(writeFile.exists()){
+                JOptionPane.showMessageDialog(this, "该文件已存在", "提示", JOptionPane.INFORMATION_MESSAGE);
+            }else {
+                writeFile.createNewFile();
+                BufferedWriter out = new BufferedWriter(new FileWriter(writeFile));
+                out.write("日志来源:\t"+rootNode.getUserObject().toString()+"\n");
+                out.write("action:\t"+ action +"\n");
+                out.write("keyWord:\t");
+                for(String str:keyWord){
+                    out.write(str+"\t");
+                }
+                out.write("\n");
+                visitNodes(node,out);
+                out.close();
+            }
+
+        }catch (IOException ioe){
+            System.out.println("ERROR"+ioe.toString());
+        }
+    }
+
+    public void visitNodes(DefaultMutableTreeNode node,BufferedWriter writer){
+
+        try{
+            writer.write(node.getUserObject().toString()+"\n");
+            writer.flush();
+
+        }catch (Exception e3){
+            System.out.println("ERROR"+e3.toString());
+        }
+
+        if(node.getChildCount()>0){
+            for (Enumeration e = node.children();e.hasMoreElements();){
+                DefaultMutableTreeNode n = (DefaultMutableTreeNode) e.nextElement();
+                visitNodes(n,writer);
+            }
+        }else {
+
+        }
+    }
+
     public void itemStateChanged(ItemEvent e){
+        /*
         if(e.getStateChange()== ItemEvent.SELECTED){
-            keyWord = text.getText().trim();
-            if(keyWord.length()==0){
+            keyWord = text.getText().trim().split("|");
+            if(keyWord.length==0){
                 return;
             }
             Enumeration ee=rootNode.children();
@@ -256,6 +335,7 @@ public class Main extends JPanel implements ActionListener, ItemListener {
             }
             treeModel.reload();
         }
+        */
     }
     class MyTreeModelListener implements TreeModelListener {
         public void treeNodesChanged(TreeModelEvent e) {
@@ -278,14 +358,9 @@ public class Main extends JPanel implements ActionListener, ItemListener {
 
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("DynamicTreeDemo");
+        JFrame frame = new JFrame("Log");
         frame.setLocation(400,300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        String testStr = ".*"+"(?i)"+"org"+".*"  ;
-        String str ="2019-07-18 00:00:00 [ERROR] [org.springframework.scheduling.quartz.SchedulerFactoryBean#0_Worker-5] [com.virtual.plat.server.task.SyncCrlJobController::start] JobCrlSync Start." ;
-        System.out.println(str.matches(".*(?i)org.*")  );
-
-
         //Create and set up the content pane.
         Main newContentPane = new Main();
         newContentPane.setOpaque(true); //content panes must be opaque
@@ -362,6 +437,7 @@ public class Main extends JPanel implements ActionListener, ItemListener {
         if (shouldBeVisible) {
             tree.scrollPathToVisible(new TreePath(childNode.getPath()));
         }
+
         return childNode;
     }
 
